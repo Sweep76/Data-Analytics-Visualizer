4ea77e19f671a1b3c5ce629f2cdeecbab90fded8
Sweep <joshuapatrickchiu76@gmail.com>
2024-01-17 20:54:04 +0800

    [Added main.py for test running]

---
+++ Demographic-Data-Analyzer/main.py
@@ -0,0 +1,8 @@
+import demographic_data_analyzer
+from unittest import main
+
+# Test function by calling here
+demographic_data_analyzer.calculate_demographic_data()
+
+#run unit test automatically
+main(module='test_module', exit=False)
